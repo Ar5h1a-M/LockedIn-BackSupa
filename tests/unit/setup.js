d8e7f8a56c330e09830faa9433976b0f67a79d99
Ar5h1a-M <2542915@students.wits.ac.za>
2025-10-17 18:13:16 +0200

    fix-git secrets+test.yml

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -10,4 +10,6 @@ process.env.SMTP_PASS = 'testpass';
 process.env.SUPABASE_URL = 'https://test.supabase.co';
 process.env.SUPABASE_SERVICE_ROLE_KEY = 'test-service-role-key';
 
+
+
 console.log('Test setup completed');
